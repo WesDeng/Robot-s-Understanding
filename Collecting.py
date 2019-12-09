5ccb561dfb2ca26582a41239a1af29a472888eb8
Wesley Deng <wesley1016@berkeley.edu>
2019-12-09 03:43:36 -0800

    change pic1

--- Collecting.py
+++ Collecting.py
@@ -42,8 +42,12 @@ overlay = 'overlay.jpg'
 
 image_list = ['image_3.jpg', 'image_4.jpg', 'dummy_image.jpg']
 
-picture_list = ['a.jpg', 'b.jpg', 'c.jpg', 'd.jpg', 'e.jpg', 'f.jpg', 'g.jpg',
-                'h.jpg', 'i.jpg', 'j.jpg', 'k.jpg', 'l.jpg', 'm.jpg', 'n.jpg']
+picture_list = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg',
+                '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg',
+                '15.jpg', '16.jpg', '17.jpg','18.jpg', '19.jpg', '20.jpg',
+                '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg',
+                '28.jpg', '29.jpg', '30.jpg', '31.jpg', '32.jpg', '33.jpg', '34.jpg',
+                '35.jpg', '36.jpg']
 
 
 
@@ -82,29 +86,29 @@ def edge(image_name):
     image = Image.open(image_name)
     imageWithEdges = image.filter(ImageFilter.CONTOUR)
     imageWithEdges.save(image_name)
-    
+
 def emboss(image_name):
     image = Image.open(image_name)
     imageWithEdges = image.filter(ImageFilter.EMBOSS)
     imageWithEdges.save(image_name)
-    
+
 def box(image_name):
     image = Image.open(image_name)
     imageWithEdges = image.filter(ImageFilter.BoxBlur(36))
     imageWithEdges.save(image_name)
-    
+
 def kernel(image_name):
     image = Image.open(image_name)
     imageWithEdges = image.filter(ImageFilter.RankFilter((3,3), 9/2))
     imageWithEdges.save(image_name)
-    
+
 def color(image_name):
     img = Image.open(image_name)
     for i in range(0, img.size[0]-1):
         for j in range(0, img.size[1]-1):
             pixelColorVals = img.getpixel((i,j))
-            redPixel    = 255 - pixelColorVals[0] 
-            greenPixel  = 255 - pixelColorVals[1] 
+            redPixel    = 255 - pixelColorVals[0]
+            greenPixel  = 255 - pixelColorVals[1]
             bluePixel   = 255 - pixelColorVals[2]
             img.putpixel((i,j),(redPixel, greenPixel, bluePixel))
     img.save(image_name)
@@ -116,7 +120,7 @@ def image_overlay(image, to_add):
     overlay = Image.open(to_add).convert('RGB')
     new_img = Image.blend(background, overlay, 0.6)
     new_img.save(image)
-    
+
 
 
 #background = Image.open("bg.png")
@@ -140,27 +144,18 @@ def main():
     clock = pg.time.Clock()
 
     #pg.display.flip()
-    
-
 
     camera = picamera.PiCamera()
     pg.init()
     pg.mixer.init()
     pg.mixer.set_num_channels(3)
 
+
     channel_real = pg.mixer.Channel(0) # Real time streaming.
     channel_effect = pg.mixer.Channel(1) # Sound effect
     channel_record = pg.mixer.Channel(2) # Pre recording.
 
-    # Variable for GUI
-
-
-    # Create a pygame GUI with layout, four picture.
-    # 1. Picture manipulating. Blinking.
-
-    # Recording the realtime radio and play back.
-
-
+    channel_real.play(pg.mixer.Sound('Interpreting.mp3'))
 
     while True:
 
@@ -176,20 +171,20 @@ def main():
         camera.capture('image_2.jpg')
         #print('take image')
         camera.capture(random.choice(image_list))
-        
+
         print('take again')
         camera.stop_preview()
-        
+
         emboss('image_1.jpg')
         box('image.jpg')
         image_overlay('image_4.jpg', 'image_3.jpg')
         edge('image_2.jpg')
-        
+
 
         # create picture signal and put it onto the Py GUI
         print('load...')
-        pic1 = pg.image.load('image_1.jpg')
-        pic2 = pg.image.load('image_2.jpg')
+        pic1 = pg.image.load('pixels/' + random.choice(picture_list))
+        pic2 = pg.image.load('image_1.jpg')
         pic3 = pg.image.load('image.jpg')
         pic4 = pg.image.load('image_4.jpg')
         print('load finished')
